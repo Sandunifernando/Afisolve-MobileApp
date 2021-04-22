7e8cb1d772793239c35627dfacaa7cb56b8c3ab6
bhsmf <bhsmfernando1120@gmail.com>
2021-04-22 09:09:39 +0530

    test

--- App.js
+++ App.js
@@ -14,13 +14,16 @@ import SigninPage from './app/components/Authentication/Signin';
 import NotificationsScreen from './app/components/Screens/NotificationScreen';
 import ComplaintSubmission from './app/components/Screens/ComplaintSubmission';
 import MainTabNavigator from './app/components/Navigations/MainTabNavigator';
+import {DrawerContent} from './app/components/Navigations/DrawerContent';
 
 const Drawer = createDrawerNavigator();
 const App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
+      {/*<Drawer.Navigator drawerContent={props => <DrawerContent {...props}/>}>*/}
+
+      <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Signin" component={SigninPage} />
         <Drawer.Screen name="DashboardDrawer" component={MainTabNavigator} />
         <Drawer.Screen name="SubmitComplaint" component={ComplaintSubmission} />
